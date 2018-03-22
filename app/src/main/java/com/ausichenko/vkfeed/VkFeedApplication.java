ee0549bbb403aecfe20966f1b9897a16d9e8f9d3
Andrey Usichenko <usichenko.andrey@gmail.com>
2018-03-22 23:39:36 +0700

    Add token tracker.

--- app/src/main/java/com/ausichenko/vkfeed/VkFeedApplication.java
+++ app/src/main/java/com/ausichenko/vkfeed/VkFeedApplication.java
@@ -1,15 +1,30 @@
 package com.ausichenko.vkfeed;
 
 import android.app.Application;
+import android.content.Intent;
 
+import com.ausichenko.vkfeed.mvp.splash.SplashActivity;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKSdk;
 
 public class VkFeedApplication extends Application {
 
+    private VKAccessTokenTracker mVkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
-
-        VKSdk.initialize(getApplicationContext());
+        mVkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
     }
 }
