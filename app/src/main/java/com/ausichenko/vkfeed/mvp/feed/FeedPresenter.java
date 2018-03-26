becd0428e79abb7a42da60905cbd6d575f8ba811
Andrey Usichenko <usichenko.andrey@gmail.com>
2018-03-26 13:59:59 +0700

    Add loading feed.

--- app/src/main/java/com/ausichenko/vkfeed/mvp/feed/FeedPresenter.java
+++ app/src/main/java/com/ausichenko/vkfeed/mvp/feed/FeedPresenter.java
@@ -1,13 +1,38 @@
 package com.ausichenko.vkfeed.mvp.feed;
 
+import android.util.Log;
+
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.ausichenko.vkfeed.data.api.methods.VKApiNewsfeed;
 import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
 
 @InjectViewState
 public class FeedPresenter extends MvpPresenter<FeedView> {
 
     void loadFeed() {
+        VKApiNewsfeed apiNewsfeed = new VKApiNewsfeed();
+        VKParameters parameters = new VKParameters();
+        VKRequest request = apiNewsfeed.get(parameters);
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                Log.d("FeedPresenter", "onComplete()");
+            }
 
+            @Override
+            public void onError(VKError error) {
+                Log.d("FeedPresenter", "onError()");
+            }
+
+            @Override
+            public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded, long bytesTotal) {
+                Log.d("FeedPresenter", "onProgress()");
+            }
+        });
     }
 }
--- app/src/main/java/com/ausichenko/vkfeed/mvp/splash/SplashActivity.java
+++ app/src/main/java/com/ausichenko/vkfeed/mvp/splash/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends MvpAppCompatActivity implements SplashView {
 
     @Override
     public void startLoginActivity() {
-        VKSdk.login(this);
+        VKSdk.login(this, "wall", "friends");
     }
 
     @Override
